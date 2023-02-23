d38cf7a77f21647458c5e281a7278994c41b2434
Inis Kryeziu <inis.kryeziu2000@gmail.com>
2023-02-23 17:30:26 +0100

    Cart component, Modal via reactPortal, useContext, useRedcer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCardHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCardHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCardHandler} />}
+      <Header onShowCart={showCardHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
